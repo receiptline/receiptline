db89dc4126d5a31641cdb21a7167856b6115ad57
eggplants <w10776e8w@yahoo.co.jp>
2022-03-30 03:05:01 +0900

    fix: import -> require

--- bin/receiptline.js
+++ bin/receiptline.js
@@ -12,5 +12,5 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { cli } from '../src/cli.js';
+const { cli } = require('../src/cli.js');
 cli();
