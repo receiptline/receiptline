299345650a7455b184fdc0ab8a085deaf5e86509
eggplants <w10776e8w@yahoo.co.jp>
2022-03-30 03:17:40 +0900

    add: task to format with prettier to script

--- bin/receiptline.js
+++ bin/receiptline.js
@@ -12,5 +12,5 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-const { cli } = require('../src/cli.js');
+const {cli} = require('../src/cli.js');
 cli();
--- src/cli.js
+++ src/cli.js
@@ -11,44 +11,46 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-const { InvalidArgumentError, Command, Option } = require("commander");
-const { transform, commands } = require("../lib/receiptline.js");
-const { statSync, readFile, writeFile } = require("fs");
-const { basename, dirname } = require("path");
+const {InvalidArgumentError, Command, Option} = require('commander');
+const {transform, commands} = require('../lib/receiptline.js');
+const {statSync, readFile, writeFile} = require('fs');
+const {basename, dirname} = require('path');
 
 const encodings = [
-    "multilingual",
-    "cp437",
-    "cp852",
-    "cp858",
-    "cp860",
-    "cp863",
-    "cp865",
-    "cp866",
-    "cp1252",
-    "cp932",
-    "shiftjis",
-    "cp936",
-    "gb18030",
-    "cp949",
-    "ksc5601",
-    "cp950",
-    "big5",
+    'multilingual',
+    'cp437',
+    'cp852',
+    'cp858',
+    'cp860',
+    'cp863',
+    'cp865',
+    'cp866',
+    'cp1252',
+    'cp932',
+    'shiftjis',
+    'cp936',
+    'gb18030',
+    'cp949',
+    'ksc5601',
+    'cp950',
+    'big5',
 ];
 
 module.exports = {
     cli: () => {
-        const program = new Command("receiptline");
-        program.description("receiptline CLI").version(
-            "receiptline version 1.8.0\n"
-            + "(C) 2019-, Open Foodservice System Consortium\n"
-            + "Licensed under the Apache License, Version 2.0\n"
-            + "https://github.com/receiptline/receiptline"
-        );
+        const program = new Command('receiptline');
+        program
+            .description('receiptline CLI')
+            .version(
+                'receiptline version 1.8.0\n' +
+                    '(C) 2019-, Open Foodservice System Consortium\n' +
+                    'Licensed under the Apache License, Version 2.0\n' +
+                    'https://github.com/receiptline/receiptline'
+            );
         program
             .option(
-                "-c, --cpl <number>",
-                "characters per line (24-48)",
+                '-c, --cpl <number>',
+                'characters per line (24-48)',
                 (v, _) => {
                     let i = parseInt(v);
                     if (24 <= i && i <= 48) {
@@ -62,17 +64,17 @@ module.exports = {
                 48
             )
             .addOption(
-                new Option("-e, --encoding <encoding>", "encoding")
-                    .default("cp437")
+                new Option('-e, --encoding <encoding>', 'encoding')
+                    .default('cp437')
                     .choices(encodings)
             )
-            .option("-u, --upside-down", "upside down")
-            .option("-s, --spacing", "line spacing")
-            .option("-c, --no-cutting", "no paper cutting")
-            .option("-G, --gradient", "image processing for photos")
+            .option('-u, --upside-down', 'upside down')
+            .option('-s, --spacing', 'line spacing')
+            .option('-c, --no-cutting', 'no paper cutting')
+            .option('-G, --gradient', 'image processing for photos')
             .option(
-                "-g, --gamma <number>",
-                "image gamma correction (0.1-10.0)",
+                '-g, --gamma <number>',
+                'image gamma correction (0.1-10.0)',
                 (v, _) => {
                     let f = parseFloat(v);
                     if (0.1 <= f && f <= 10.0) {
@@ -86,8 +88,8 @@ module.exports = {
                 1.8
             )
             .option(
-                "-b, --threshold <number>",
-                "image thresholding (0-255)",
+                '-b, --threshold <number>',
+                'image thresholding (0-255)',
                 (v, _) => {
                     let i = parseInt(v);
                     if (0 <= i && i <= 255) {
@@ -101,16 +103,19 @@ module.exports = {
                 128
             )
             .addOption(
-                new Option("-p, --printer <command>", "printer control language")
-                    .default("svg")
+                new Option(
+                    '-p, --printer <command>',
+                    'printer control language'
+                )
+                    .default('svg')
                     .choices(Object.getOwnPropertyNames(commands))
             )
-            .option("-o, --output <path>", "output file")
-            .argument("[source]", "source file");
+            .option('-o, --output <path>', 'output file')
+            .argument('[source]', 'source file');
         program.configureHelp({
             helpWidth: 100,
             sortOptions: true,
-        })
+        });
 
         program.parse();
         const opts = program.opts();
@@ -119,19 +124,19 @@ module.exports = {
         const argn = args.length;
         const doc = (() => {
             if (argn === 0) {
-                var input = "";
+                var input = '';
                 process.stdin.resume();
-                process.stdin.setEncoding("utf8");
-                process.stdin.on("data", function (chunk) {
+                process.stdin.setEncoding('utf8');
+                process.stdin.on('data', function (chunk) {
                     input += chunk;
                 });
-                process.stdin.on("end", function () {
+                process.stdin.on('end', function () {
                     return input;
                 });
             } else if (argn === 1) {
                 const f = args[0];
                 if (statSync(f).isFile()) {
-                    readFile(f, "utf-8", (err, input) => {
+                    readFile(f, 'utf-8', (err, input) => {
                         if (err) {
                             throw err;
                         } else {
@@ -167,16 +172,16 @@ module.exports = {
         } else {
             const outDir = dirname(opts.output);
             const outFile = basename(opts.output);
-            if (outDir !== "" && !statSync(outDir).isDirectory()) {
+            if (outDir !== '' && !statSync(outDir).isDirectory()) {
                 throw new InvalidArgumentError(
                     `Invarid parent directory of output file ('${outDir}' is not a directory)`
                 );
-            } else if (outFile !== "" && statSync(outFile).isDirectory()) {
+            } else if (outFile !== '' && statSync(outFile).isDirectory()) {
                 throw new InvalidArgumentError(
                     `Invarid output file ('${outFile}' is a directory)`
                 );
             } else {
-                writeFile(opts.output, data, { flag: "w" }, (err) => {
+                writeFile(opts.output, data, {flag: 'w'}, err => {
                     if (err) {
                         throw err;
                     } else {
@@ -185,5 +190,5 @@ module.exports = {
                 });
             }
         }
-    }
-}
+    },
+};
